9f49c01a8dbb51f96cc6832d5d482d6d2b508358
gaurav9969351313 <gaurav.talele@rebit.org.in>
2021-10-16 11:50:51 +0530

    added interceptor for command validation

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ https://github.com/simplyi?tab=repositories
 My Account → Settings → Developer settings → Personal access tokens → Generate new token.
 
 
-git remote set-url origin https://<token>@github.com/gaurav9969351313/gt-microservices-boilerplate
+git remote set-url origin https://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@github.com/gaurav9969351313/gt-microservices-boilerplate
 ============================================================================================================
--- products-service/src/main/java/com/rebit/productsservice/ProductsServiceApplication.java
+++ products-service/src/main/java/com/rebit/productsservice/ProductsServiceApplication.java
@@ -1,8 +1,13 @@
 package com.rebit.productsservice;
 
+import com.rebit.productsservice.command.interceptors.CreateProductCommandInterceptor;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +17,12 @@ public class ProductsServiceApplication {
 		SpringApplication.run(ProductsServiceApplication.class, args);
 	}
 
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context, 
+			CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
+		
+	}
 }
 
 
