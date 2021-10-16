0736c1ff9866ed32dce85418c8c6fd0786de0fc9
gaurav9969351313 <gaurav.talele@rebit.org.in>
2021-10-16 13:57:16 +0530

    added order service

---
+++ orders-service/src/test/java/com/rebit/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebit.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
